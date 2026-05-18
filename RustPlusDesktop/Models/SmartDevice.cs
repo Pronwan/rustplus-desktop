ddebdea09085b53dbf43ec888a7340ac5374956b
Carsten Podszun <carsten.podszun@gmail.com>
2026-05-18 07:33:23 +0200

    Smart Alarm rework with PiP Alert / Popup / Audio

--- RustPlusDesktop/Models/SmartDevice.cs
+++ RustPlusDesktop/Models/SmartDevice.cs
@@ -219,6 +219,13 @@ public class SmartDevice : INotifyPropertyChanged
         set { if (_audioEnabled != value) { _audioEnabled = value; OnProp(); } }
     }
 
+    private bool _overlayEnabled = true;
+    public bool OverlayEnabled
+    {
+        get => _overlayEnabled;
+        set { if (_overlayEnabled != value) { _overlayEnabled = value; OnProp(); } }
+    }
+
     private string? _audioFilePath;
     public string? AudioFilePath
     {
