a9aace5c6ea17b68a450db35439e97d4a389c4b6
JawadYzbk <jawadyazbek@gmail.com>
2026-08-05 20:41:11 +0300

    Support Remove as Small/Large Oil Rig Trigger context menu option and prevent single alarm dual assignment

--- RustPlusDesktop/Views/MainWindow/Devices/MainWindow.Devices.cs
+++ RustPlusDesktop/Views/MainWindow/Devices/MainWindow.Devices.cs
@@ -1319,7 +1319,7 @@ private async void BtnDeviceRefresh_Click(object sender, RoutedEventArgs e)
         {
             if (isThisSmall)
             {
-                menuSmall.Header = Properties.Resources.UiRemoveAsSmallOilRigTrigger;
+                menuSmall.Header = Properties.Resources.GetString("UiRemoveAsSmallOilRigTrigger");
                 menuSmall.Tag = "REMOVE";
                 menuSmall.Visibility = Visibility.Visible;
             }
@@ -1329,7 +1329,7 @@ private async void BtnDeviceRefresh_Click(object sender, RoutedEventArgs e)
             }
             else
             {
-                menuSmall.Header = Properties.Resources.UiSetAsSmallOilRigTrigger;
+                menuSmall.Header = Properties.Resources.GetString("UiSetAsSmallOilRigTrigger");
                 menuSmall.Tag = "SET";
                 menuSmall.Visibility = Visibility.Visible;
             }
@@ -1339,7 +1339,7 @@ private async void BtnDeviceRefresh_Click(object sender, RoutedEventArgs e)
         {
             if (isThisLarge)
             {
-                menuLarge.Header = Properties.Resources.UiRemoveAsLargeOilRigTrigger;
+                menuLarge.Header = Properties.Resources.GetString("UiRemoveAsLargeOilRigTrigger");
                 menuLarge.Tag = "REMOVE";
                 menuLarge.Visibility = Visibility.Visible;
             }
@@ -1349,7 +1349,7 @@ private async void BtnDeviceRefresh_Click(object sender, RoutedEventArgs e)
             }
             else
             {
-                menuLarge.Header = Properties.Resources.UiSetAsLargeOilRigTrigger;
+                menuLarge.Header = Properties.Resources.GetString("UiSetAsLargeOilRigTrigger");
                 menuLarge.Tag = "SET";
                 menuLarge.Visibility = Visibility.Visible;
             }
