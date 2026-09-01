11301efb1b075ea76214616779c84e061ce2bd55
JawadYzbk <jawadyazbek@gmail.com>
2026-09-02 01:50:52 +0300

    feat: add settings toggle to enable/disable traffic monitor feature

--- RustPlusDesktop/Views/MainWindow/Map/AppSettingsOverlay.xaml.cs
+++ RustPlusDesktop/Views/MainWindow/Map/AppSettingsOverlay.xaml.cs
@@ -623,6 +623,7 @@ namespace RustPlusDesk.Views
             ChkAutoConnect.IsChecked = TrackingService.AutoConnectEnabled;
             ChkCloseToTray.IsChecked = TrackingService.CloseToTrayEnabled;
             ChkHideConsole.IsChecked = TrackingService.HideConsole;
+            ChkTrafficMonitor.IsChecked = TrackingService.TrafficMonitorEnabled;
             ChkStreamerMode.IsChecked = TrackingService.MapAbbreviateNames;
             
             TxtDiscordWebhookUrl.Text = TrackingService.DiscordWebhookUrl;
@@ -827,6 +828,7 @@ namespace RustPlusDesk.Views
             TrackingService.AutoConnectEnabled = ChkAutoConnect.IsChecked == true;
             TrackingService.CloseToTrayEnabled = ChkCloseToTray.IsChecked == true;
             TrackingService.HideConsole = ChkHideConsole.IsChecked == true;
+            TrackingService.TrafficMonitorEnabled = ChkTrafficMonitor.IsChecked == true;
             TrackingService.MapAbbreviateNames = ChkStreamerMode.IsChecked == true;
             
             if (CmbMapScalingMode != null && CmbMapScalingMode.SelectedIndex >= 0)
